3aa0d7516c7603f4a90c2d171b5afa6e49143cd2
mango7431 <rsm5065@naver.com>
2024-05-17 10:41:49 +0900

    req2ndprocess postgresql 작업 테스트 필요

--- src/req2ndprocess/req2ndprocess.go
+++ src/req2ndprocess/req2ndprocess.go
@@ -1,11 +1,11 @@
 package req2ndprocess
 
 import (
-	"webagent/src/config"
 	"database/sql"
-	"sync"
-	"webagent/src/databasepool"
 	s "strings"
+	"sync"
+	"webagent/src/config"
+	"webagent/src/databasepool"
 )
 
 func Process() {
@@ -24,11 +24,11 @@ func resProcess(wg *sync.WaitGroup) {
 	var db = databasepool.DB
 	var conf = config.Conf
 	var stdlog = config.Stdlog
-	
+
 	var cnt sql.NullString
-	
+
 	var reqquery = "SELECT count(1) as cnt FROM " + conf.REQTABLE2 + " where remark3 = 'Y'"
-	
+
 	err := db.QueryRow(reqquery).Scan(&cnt)
 
 	if err != nil {
@@ -36,27 +36,26 @@ func resProcess(wg *sync.WaitGroup) {
 		stdlog.Println(err)
 		stdlog.Fatal(reqquery)
 	}
-	 
-	if !s.EqualFold(cnt.String, "0") { 
+
+	if !s.EqualFold(cnt.String, "0") {
 		stdlog.Println("2ND 테이블에서 Req 로 복사 시작 - ", cnt)
-		
-		resp, _ := db.Exec("update " + conf.REQTABLE2 + " as t2 set t2.remark3 = 'P' where t2.remark3 = 'Y' and not exists (select 1 from " + conf.REQTABLE1 + " as t1 where t1.msgid = replace(t2.msgid,'AT', '') )")
-		
+
+		resp, _ := db.Exec("update " + conf.REQTABLE2 + " as t2 set remark3 = 'P' where t2.remark3 = 'Y' and not exists (select 1 from " + conf.REQTABLE1 + " as t1 where t1.msgid = replace(t2.msgid,'AT', '') )")
+
 		upp, _ := resp.RowsAffected()
-		
+
 		resins, err2 := db.Exec("insert into " + conf.REQTABLE1 + " select * from " + conf.REQTABLE2 + " as t2 where remark3 = 'P'")
-		
+
 		if err2 != nil {
 			stdlog.Println("2ND 테이블에서 Req 로 복사 중 오류 발생 - Update P : ", upp)
 		} else {
-		
+
 			insp, _ := resins.RowsAffected()
-			
-			resn, _ :=db.Exec("update " + conf.REQTABLE2 + " set remark3 = 'N' where remark3 = 'P'")
+
+			resn, _ := db.Exec("update " + conf.REQTABLE2 + " set remark3 = 'N' where remark3 = 'P'")
 			upn, _ := resn.RowsAffected()
 			stdlog.Println("2ND 테이블에서 Req 로 복사 끝 - Update P / Insert / Update N : ", upp, insp, upn)
 		}
-		
-		
+
 	}
 }
