e22f65b3e5ab7183583bb79a87a186111ee85115
mango7431 <rsm5065@naver.com>
2024-05-17 10:10:05 +0900

    add webc.. update msgid in function

--- src/common/common.go
+++ src/common/common.go
@@ -1,6 +1,10 @@
 package common
 
-import "reflect"
+import (
+	"database/sql"
+	"fmt"
+	"reflect"
+)
 
 func GetRcsColumnPq(mtype interface{}) []string {
 	t := reflect.TypeOf(mtype)
@@ -9,4 +13,19 @@ func GetRcsColumnPq(mtype interface{}) []string {
 		columns[i] = t.Field(i).Tag.Get("db")
 	}
 	return columns
+}
+
+func UpdateProcFlag(tx *sql.Tx, tableName string, ids []interface{}) error {
+	commastr := fmt.Sprintf("update %s set proc_flag='N' where msgid in (", tableName)
+	for i := range ids {
+		if i == 0 {
+			commastr += fmt.Sprintf("$%d", i+1)
+		} else {
+			commastr += fmt.Sprintf(", $%d", i+1)
+		}
+	}
+	commastr += ")"
+
+	_, err := tx.Exec(commastr, ids...)
+	return err
 }
